b5c96e2380e23cf850bb9978965e166b0c9c20db
jiashunx <wenjiashun@163.com>
2020-12-04 19:13:10 +0800

    import commons-cli depedency, help to parse command line arguments.

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 
 ### SimpleFileServer
 
-- 项目简介：基于masker-rest-framework开发的简易文件服务器（参考Python的SimpleHttpServer实现），可以作为简易文件服务器使用.
+- 项目简介：基于 [masker-rest][1] 开发的简易文件服务器（参考Python的SimpleHttpServer实现），可以作为简易文件服务器使用.
 
-- 技术栈：JDK8+、masker-rest、Thymeleaf
+- 技术栈：JDK8+、[masker-rest][1]、Thymeleaf
 
 - 计划实现功能：
 
    - 文件上传（单个、多个）
    - 文件下载（单个、多个）
    - 文件分享下载短链接地址生成
+
+[1]: https://github.com/jiashunx/masker-rest
--- src/main/java/io/github/jiashunx/simplefileserver/SimpleFileServerBoot.java
+++ src/main/java/io/github/jiashunx/simplefileserver/SimpleFileServerBoot.java
@@ -1,11 +1,31 @@
 package io.github.jiashunx.simplefileserver;
 
 import io.github.jiashunx.masker.rest.framework.MRestServer;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
 
 public class SimpleFileServerBoot {
 
-    public static void main(String[] args) {
-        new MRestServer(8080).start();
+    private static final Logger logger = LoggerFactory.getLogger(SimpleFileServerBoot.class);
+
+    public static void main(String[] args) throws ParseException {
+        logger.info("arguments: {}", Arrays.asList(args));
+        CommandLineParser commandLineParser = new BasicParser();
+        Options options = new Options();
+        // java -jar xx.jar -p 8080 --port 8080
+        options.addOption("p", "port", true, "server port(default 8080)");
+        CommandLine commandLine = commandLineParser.parse(options, args);
+        int serverPort = 8080;
+        if (commandLine.hasOption('p')) {
+            serverPort = Integer.parseInt(commandLine.getOptionValue('p'));
+        }
+        if (commandLine.hasOption("port")) {
+            serverPort = Integer.parseInt(commandLine.getOptionValue("port"));
+        }
+        new MRestServer(serverPort).start();
     }
 
 }
